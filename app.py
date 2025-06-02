e8d5c8a9c1babe7eb106640bc604648441450e29
dewarder <90405195+dewarder@users.noreply.github.com>
2025-06-02 02:48:36 -0400

    Add working Flask app and Procfile for Render

--- app.py
+++ app.py
@@ -1,2 +1,12 @@
 # Minor change to trigger Git
 # Flask requirement
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return 'Welcome to Rhythm & Reality! 🚀'
+
+if __name__ == '__main__':
+    app.run(debug=True)
