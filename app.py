57167bc9a6cbcbb7067c339d254cd0d8060c8648
dewarder <90405195+dewarder@users.noreply.github.com>
2025-06-02 03:42:48 -0400

    Add homepage, about, and music pages

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
-# Minor change to trigger Git
-# Flask requirement
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Welcome to Rhythm & Reality! 🚀'
+    return render_template("index.html")
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+@app.route('/music')
+def music():
+    return render_template("music.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
